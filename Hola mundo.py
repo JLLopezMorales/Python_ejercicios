42c16bcddae1a6e6334615956e53cfc815784bab
JLLopezMorales <jorgerunner97@gmail.com>
2026-06-15 14:00:29 +0200

    cambios

--- Hola mundo.py
+++ Hola mundo.py
@@ -4,6 +4,6 @@ for i in range(5):
     print("Este es el mensaje número " + str(i+1))
 if i == 4:
     print("¡Gracias por usar el programa, " + nombre + "! Esperamos que tengas una excelente experiencia.") 
-   y=input("¿Deseas continuar usando el programa? (sí/no): ")
+y=input("¿Deseas continuar usando el programa? (sí/no): ")
 if y.lower() == "sí":
     print("¡Genial! Continuemos con el programa.") 
