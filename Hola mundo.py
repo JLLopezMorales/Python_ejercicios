6c898d9a63f11f59eaf6afaa3c388537a0d82f4d
JLLopezMorales <jorgerunner97@gmail.com>
2026-06-15 13:55:05 +0200

    git add "Hola mundo.py"
    git commit -m "Actualizado el Hola mundo con nuevos cambios"
    
    git push
    git add "Hola mundo.py"
    
    git commit -m "Actualizado el Hola mundo con nuevos cambios"
    
    git push

--- Hola mundo.py
+++ Hola mundo.py
@@ -2,3 +2,5 @@ nombre=input("Ingrese su nombre: ")
 print("Hola, " + nombre + "! Bienvenido/a al programa, es tu primera vez usando VS Code.")
 for i in range(5):
     print("Este es el mensaje número " + str(i+1))
+if i == 4:
+    print("¡Gracias por usar el programa, " + nombre + "! Esperamos que tengas una excelente experiencia.") 
